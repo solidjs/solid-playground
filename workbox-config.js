4bac654e5bb83cae44a1a87049c46ff54c6c890c
Alexandre <alexandre.mouton-brady@airweb.fr>
2020-10-03 17:10:32 +0200

    :arrow_up: Update dependencies

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  "globDirectory": "dist/",
-  "globPatterns": [
-    "**/*.{js,css,html,svg,png,webmanifest}"
-  ],
-  "swDest": "dist/sw.js"
+  globDirectory: "dist/",
+  globPatterns: ["**/*.{js,css,html,svg,png,webmanifest}"],
+  swDest: "dist/sw.js",
+  skipWaiting: true,
 };
