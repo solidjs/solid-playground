dda6d23c58052af45c21df40e129a2be20734171
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2020-12-21 13:18:50 +0100

    :pencil: :sparkles: :zap: Debounce compilation which lead to better UX & document the code

---
+++ src/utils/debounce.ts
@@ -0,0 +1,12 @@
+export function debounce(
+  callback: (...params: unknown[]) => unknown,
+  wait: number
+) {
+  let timeout: NodeJS.Timeout;
+
+  return (...args: unknown[]) => {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => callback.apply(context, args), wait);
+  };
+}
