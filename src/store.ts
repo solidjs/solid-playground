5b2acce3dc7da338ef65e1b2cd58142a573d2b14
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2020-12-23 13:52:55 +0100

    :recycle: Change default example from Hello World to a Counter

--- src/store.ts
+++ src/store.ts
@@ -4,10 +4,24 @@ import { uid, parseHash } from "./utils";
 const defaultTabs: Tab[] = [
   {
     id: uid(),
-    name: "app",
+    name: "main",
     type: "tsx",
     source:
-      "import { render } from 'solid-js/web'\n\nconst App = () => <h1>Hello world</h1>\n\nrender(App, document.getElementById('app'))",
+      "import { render } from 'solid-js/web';\n" +
+      "import { createSignal } from 'solid-js';\n" +
+      "\n" +
+      "function Counter() {\n" +
+      "  const [count, setCount] = createSignal(0);\n" +
+      "  const increment = () => setCount(count() + 1)\n" +
+      "\n" +
+      "  return (\n" +
+      '    <button type="button" onClick={increment}>\n' +
+      "      {count()}\n" +
+      "    </button>\n" +
+      "  );\n" +
+      "}\n" +
+      "\n" +
+      "render(() => <Counter />, document.getElementById('app'))\n",
   },
 ];
 
