149ae0fb3820a49bb2f3ac3a59e814d10da8c6ef
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2020-12-23 13:53:48 +0100

    :sparkles: Adding a format code option

---
+++ src/workers/formatter.ts
@@ -0,0 +1,25 @@
+// @ts-ignore
+import prettier from "prettier/esm/standalone.mjs";
+// @ts-ignore
+import parserBabel from "prettier/esm/parser-babel.mjs";
+
+function format(code: string) {
+  return prettier.format(code, {
+    parser: "babel",
+    plugins: [parserBabel],
+  });
+}
+
+self.addEventListener("message", async ({ data }) => {
+  const { event, code } = data;
+
+  switch (event) {
+    case "FORMAT":
+      // @ts-ignore
+      self.postMessage({
+        event: "RESULT",
+        code: await format(code),
+      });
+      break;
+  }
+});
