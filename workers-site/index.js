3c9f66afaaa533d5fd792bd445be4113c3d14db7
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2020-12-24 21:56:47 +0100

    :sparkles: Reworked the worker

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,80 +1,108 @@
-import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
- */
-const DEBUG = false
+// https://gist.github.com/hyamamoto/fd435505d29ebfa3d9716fd2be8d42f0#gistcomment-2800233
+function uid(str) {
+  return Array.from(str)
+    .reduce((s, c) => (Math.imul(31, s) + c.charCodeAt(0)) | 0, 0)
+    .toString();
+}
 
-addEventListener('fetch', event => {
+const corsHeaders = {
+  'Access-Control-Allow-Origin': 'https://playground.solidjs.com',
+  'Access-Control-Allow-Methods': 'GET,HEAD,PUT,POST,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+};
+
+addEventListener('fetch', (event) => {
   try {
-    event.respondWith(handleEvent(event))
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      )
+    if (event.request.method === 'OPTIONS') {
+      event.respondWith(handleOptions(event));
+    } else {
+      event.respondWith(handleEvent(event));
     }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
+  } catch {
+    event.respondWith(new Response('Internal Error', { status: 500 }));
   }
-})
+});
 
 async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
-
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
-
   try {
-    if (DEBUG) {
-      // customize caching
-      options.cacheControl = {
-        bypassCache: true,
-      }
-    }
-    return await getAssetFromKV(event, options)
+    if (event.request.method === 'PUT') return handleCreateUrl(event.request);
+    else if (event.request.method === 'GET') return handleRetrieveUrl(event);
+    else return new Response('Unsupported method', { status: 401 });
   } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        })
-
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e) {}
-    }
-
-    return new Response(e.message || e.toString(), { status: 500 })
+    return new Response(e.message || e.toString(), { status: 500 });
   }
 }
 
-/**
- * Here's one example of how to modify a request to
- * remove a specific prefix, in this case `/docs` from
- * the url. This can be useful if you are deploying to a
- * route on a zone, or if you only want your static content
- * to exist at a specific path.
- */
-function handlePrefix(prefix) {
-  return request => {
-    // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request)
-    let url = new URL(defaultAssetKey.url)
+function handleOptions({ request: { headers } }) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    const respHeaders = {
+      ...corsHeaders,
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+      'Access-Control-Allow-Headers': headers.get('Access-Control-Request-Headers'),
+    };
 
-    // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
+    return new Response(null, { headers: respHeaders });
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: { Allow: 'GET, HEAD, PUT, OPTIONS' },
+    });
+  }
+}
 
-    // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey)
+async function handleCreateUrl(request) {
+  const body = await request.json();
+
+  // Bail early if URL is missing
+  if (!body.url) {
+    return new Response('Missing param URL', { status: 400 });
+  }
+
+  // Compute a hash based on the URL
+  const id = uid(body.url);
+
+  // Create it if it doesn't exist
+  const value = await URLS.get(id);
+  if (!value) await URLS.put(id, body.url);
+
+  // And return it with proper headers
+  const response = new Response(id, { status: 200 });
+  response.headers.set('Access-Control-Allow-Origin', corsHeaders['ccess-Control-Allow-Origin']);
+  response.headers.append('Vary', 'Origin');
+
+  return response;
+}
+
+async function handleRetrieveUrl(event) {
+  const url = new URL(event.request.url);
+  const hash = url.searchParams.get('hash');
+
+  // If we don't have an hash then return the playground as is
+  if (!hash) return await getAssetFromKV(event, {});
+
+  // Attempt to retrieve the link associated with the hash
+  // or fallback to the playground as is if not found
+  const value = await URLS.get(hash);
+  if (!value) return await getAssetFromKV(event, {});
+
+  try {
+    // Make sure the value is a valid URL and redirect to the associated URL
+    const url = new URL(value);
+    return Response.redirect(url.toString(), 301);
+  } catch {
+    return await getAssetFromKV(event, {});
   }
 }
