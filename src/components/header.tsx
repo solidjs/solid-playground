23e72a1cf3901759abe3d90ec8a3e78c1020cba7
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2020-12-24 20:02:49 +0100

    :fire: Revert

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,23 +4,6 @@ import logo from 'url:../assets/images/logo.svg';
 import pkg from '../../package.json';
 
 export const Header: Component = () => {
-  function share() {
-    const url = location.href;
-
-    fetch('/', {
-      method: 'PUT',
-      body: JSON.stringify({ url }),
-    })
-      .then((r) => r.text())
-      .then((hash) => {
-        const tinyUrl = new URL(location.origin);
-        tinyUrl.searchParams.set('hash', hash);
-
-        navigator.clipboard.writeText(tinyUrl.toString());
-      })
-      .catch(console.error);
-  }
-
   return (
     <header class="md:col-span-3 p-2 flex text-sm justify-between items-center bg-brand-default text-white">
       <h1 class="flex items-center space-x-4 uppercase leading-0 tracking-widest">
@@ -31,10 +14,7 @@ export const Header: Component = () => {
       </h1>
 
       <div class="flex items-center space-x-2">
-        <button type="button" onClick={share} class="text-white -mb-1 leading-none">
-          Share
-        </button>
-        <span class="-mb-1 leading-none text-white">v{pkg.dependencies['solid-js'].slice(1)}</span>
+        <span class="-mb-1 leading-0 text-white">v{pkg.dependencies['solid-js'].slice(1)}</span>
       </div>
     </header>
   );
