54766f21da0c23cf749f0447aa23297b4ff87b57
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2020-12-26 11:50:52 +0100

    :bug: Fix issue with Firefox & Safari

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,8 +1,3 @@
-const formatter = new Intl.NumberFormat('en-US', {
-  unit: 'millisecond',
-  style: 'unit',
-});
-
 export function formatMs(ms: number) {
-  return formatter.format(ms);
+  return `${ms}ms`;
 }
