9bf89336f8650ddc342838ca30650579d4b715da
modderme123 <modderme123@gmail.com>
2021-03-05 12:26:10 -0800

    :fire: get types almost working for solid

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -11,7 +11,28 @@ import {
 
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker.js?worker';
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import sPackageJson from '/node_modules/solid-js/package.json';
+import sIndex from '/node_modules/solid-js/types/index.d.ts?raw'
+import sJsx from '/node_modules/solid-js/types/jsx.d.ts?raw'
+import sArray from '/node_modules/solid-js/types/reactive/array.d.ts?raw';
+import sMutable from '/node_modules/solid-js/types/reactive/mutable.d.ts?raw';
+import sScheduler from '/node_modules/solid-js/types/reactive/scheduler.d.ts?raw';
+import sSignal from '/node_modules/solid-js/types/reactive/signal.d.ts?raw';
+import sState from '/node_modules/solid-js/types/reactive/state.d.ts?raw';
+import sStateModifier from '/node_modules/solid-js/types/reactive/stateModifiers.d.ts?raw';
+import sComponent from '/node_modules/solid-js/types/render/component.d.ts?raw';
+import sFlow from '/node_modules/solid-js/types/render/flow.d.ts?raw';
+import sHydration from '/node_modules/solid-js/types/render/hydration.d.ts?raw';
+import sRenderIndex from '/node_modules/solid-js/types/render/index.d.ts?raw';
+import sSuspense from '/node_modules/solid-js/types/render/Suspense.d.ts?raw';
 
+import sWebPackageJson from '/node_modules/solid-js/web/package.json';
+import sClient from '/node_modules/solid-js/web/types/client.d.ts?raw';
+import sCore from '/node_modules/solid-js/web/types/core.d.ts?raw';
+import sWebIndex from '/node_modules/solid-js/web/types/index.d.ts?raw';
+import sWebJsx from '/node_modules/solid-js/web/types/jsx.d.ts?raw';
+import sServerMock from '/node_modules/solid-js/web/types/server-mock.d.ts?raw';
+import sJsxRuntime from '/node_modules/solid-js/jsx-runtime.d.ts?raw';
 const Editor: Component<Props> = (props) => {
   const [internal, external] = splitProps(props, [
     'onDocChange',
@@ -56,19 +77,39 @@ const Editor: Component<Props> = (props) => {
         return new editorWorker();
       },
     };
-    
+    let Uri =monaco.Uri;
 
     editor = monaco.editor.create(parent, {
-      value: internal.defaultValue || internal.value || '',
-      language: 'javascript',
+      model: monaco.editor.createModel(internal.defaultValue || internal.value || '', "typescript", Uri.parse("inmemory://model/main.tsx")),
     });
-
-  // monaco.languages.typescript.javascriptDefaults.addExtraLib(libsource, "/node_modules/@types/solid-js/index.d.ts");
-    // monaco.editor.createModel(libsource, "typescript", monaco.Uri.parse("ts:filename/solid-js.d.ts"));
+    
+    monaco.editor.createModel(JSON.stringify(sPackageJson), 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/package.json'));
+    monaco.editor.createModel(sIndex,'typescript',  Uri.parse('inmemory://model/node_modules/solid-js/types/index.d.ts'));
+    monaco.editor.createModel(sJsx, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/types/jsx.d.ts'));
+    monaco.editor.createModel(sArray, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/types/reactive/array.d.ts'));
+    monaco.editor.createModel(sMutable,'typescript',  Uri.parse('inmemory://model/node_modules/solid-js/types/reactive/mutable.d.ts'));
+    monaco.editor.createModel(sScheduler, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/types/reactive/scheduler.d.ts'));
+    monaco.editor.createModel(sSignal,'typescript',  Uri.parse('inmemory://model/node_modules/solid-js/types/reactive/signal.d.ts'));
+    monaco.editor.createModel(sState, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/types/reactive/state.d.ts'));
+    monaco.editor.createModel(sStateModifier, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/types/reactive/stateModifiers.d.ts'));
+    monaco.editor.createModel(sComponent,'typescript',  Uri.parse('inmemory://model/node_modules/solid-js/types/render/component.d.ts'));
+    monaco.editor.createModel(sFlow,'typescript',  Uri.parse('inmemory://model/node_modules/solid-js/types/render/flow.d.ts'));
+    monaco.editor.createModel(sHydration,'typescript',  Uri.parse('inmemory://model/node_modules/solid-js/types/render/hydration.d.ts'));
+    monaco.editor.createModel(sRenderIndex, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/types/render/index.d.ts'));
+    monaco.editor.createModel(sSuspense, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/types/render/Suspense.d.ts'));
+    monaco.editor.createModel(JSON.stringify(sWebPackageJson), 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/web/package.json'));
+    monaco.editor.createModel(sClient, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/web/types/client.d.ts'));
+    monaco.editor.createModel(sCore, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/web/types/core.d.ts'));
+    monaco.editor.createModel(sWebIndex, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/web/types/index.d.ts'));
+    monaco.editor.createModel(sWebJsx, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/web/types/jsx.d.ts'));
+    monaco.editor.createModel(sServerMock, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/web/types/server-mock.d.ts'));
+    monaco.editor.createModel(sJsxRuntime, 'typescript', Uri.parse('inmemory://model/node_modules/solid-js/jsx-runtime.d.ts'));
 
   monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-    lib: ['ESNext', 'ES2015'],
+    lib: ['es6', 'DOM', "dom.iterable"],
     target: monaco.languages.typescript.ScriptTarget.ESNext,
+    moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+    jsx: monaco.languages.typescript.JsxEmit.Preserve,
     allowNonTsExtensions: true,
   });
 
