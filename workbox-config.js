1c22b73759def27ac96ca04c66d4b7cc844eb516
Alex <amoutonbrady@gmail.com>
2021-04-05 14:17:33 +0200

    :wrench: Increased precache file size for monaco

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  globDirectory: "dist/",
-  globPatterns: ["**/*.{js,css,html,svg,png,webmanifest,woff,woff2}"],
-  swDest: "dist/sw.js",
+  globDirectory: 'dist/',
+  globPatterns: ['**/*.{js,css,html,svg,png,webmanifest,woff,woff2}'],
+  swDest: 'dist/sw.js',
   skipWaiting: true,
-  maximumFileSizeToCacheInBytes: 3_500_000,
+  maximumFileSizeToCacheInBytes: 5_000_000,
 };
