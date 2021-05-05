e4b602e596bb81bec412bafc0d5e82f970094720
Yacine Hmito <yacinehmito@users.noreply.github.com>
2021-05-05 22:35:46 +0200

    Display chevron to toggle console
    Introduced a chevron on the left of "Console", so it is made clearer for
    the user that it can be clicked to toggle the console pane.
    
    Also, changed the font-size of the console so that it matches the
    editor's.

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,6 @@
 import { Component, createEffect, createSignal, splitProps, JSX, For, Show } from 'solid-js';
+import { Icon } from '@amoutonbrady/solid-heroicons';
+import { chevronDown, chevronRight } from '@amoutonbrady/solid-heroicons/solid';
 
 export const Preview: Component<Props> = (props) => {
   const [internal, external] = splitProps(props, ['code', 'class']);
@@ -182,9 +184,19 @@ export const Preview: Component<Props> = (props) => {
           <button
             type="button"
             class="text-left font-semibold uppercase text-xs md:text-sm px-2 py-3 focus:outline-none -mb-1 md:-mb-0.5 leading-none md:leading-tight"
+            style={{ position: 'relative' }}
             onClick={() => setShowLogs(!showLogs())}
           >
-            Console ({logs().length})
+            <Icon
+              style={{
+                height: '28px',
+                position: 'absolute',
+                top: '7px',
+                left: '2px',
+              }}
+              path={showLogs() ? chevronDown : chevronRight}
+            />
+            <span class="ml-4">Console ({logs().length})</span>
           </button>
           <button
             type="button"
@@ -196,7 +208,7 @@ export const Preview: Component<Props> = (props) => {
         </div>
 
         <Show when={showLogs()}>
-          <ul class="overflow-auto px-2 divide-y">
+          <ul class="text-xs overflow-auto px-2 divide-y">
             <For each={logs()}>
               {(log) => (
                 <li
