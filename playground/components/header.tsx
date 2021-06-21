c22dfcbf4d63f90ec6ed97a2cace90095c720790
Alexandre <amoutonbrady@gmail.com>
2021-06-21 19:23:07 +0200

    :arrow_up: Update to latest solid release

--- playground/components/header.tsx
+++ playground/components/header.tsx
@@ -146,6 +146,9 @@ export const Header: Component<{
           value={props.version}
         >
           {/* TODO: Make those dynamic */}
+          <option class="text-black cursor-pointer" value="1.0.0-rc.7">
+            v1.0.0-rc.7
+          </option>
           <option class="text-black cursor-pointer" value="1.0.0-rc.6">
             v1.0.0-rc.6
           </option>
