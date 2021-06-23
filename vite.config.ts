29f4994dca9449cc7383b1e43b325f56c060a8ed
modderme123 <modderme123@gmail.com>
2021-06-23 13:26:36 -0700

    fix css paths, replace global->window

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
+export default defineConfig((env) => ({
   plugins: [solidPlugin()],
   define: {
     'process.env.BABEL_TYPES_8_BREAKING': 'true',
     'process.env.NODE_DEBUG': 'false',
+    ...(env.command == 'build' ? {} : { global: 'globalThis' }),
   },
   build: {
     target: 'esnext',
@@ -15,4 +16,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
