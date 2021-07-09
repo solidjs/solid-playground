d52fa984cc1c3a41d3ac37c99500437e6c32649c
istareatscreens <schar138@uottawa.ca>
2021-07-08 22:35:02 -0400

    Style mobile menu buttons
    Add grey hover to be similar to version drop down
    
    Remove active highlight and rounding on focus and active
    
    Remove border-radius from buttons so they fill the menu

--- playground/components/header.tsx
+++ playground/components/header.tsx
@@ -96,6 +96,10 @@ export const Header: Component<{
               type="button"
               onClick={props.toggleDark}
               class="text-black md:text-white flex flex-row space-x-2 items-center md:px-3 px-2 py-2 focus:outline-none focus:ring-1 rounded text-white opacity-80 hover:opacity-100"
+              classList={{
+                'rounded-none	active:bg-gray-300 hover:bg-gray-300 focus:outline-none focus:highlight-none active:highlight-none focus:ring-0 active:outline-none':
+                  showMenu(),
+              }}
               title="Toggle dark mode"
             >
               <Show when={props.dark} fallback={<Icon path={moon} class="h-6" />}>
@@ -106,6 +110,10 @@ export const Header: Component<{
 
             <label
               class="text-black md:text-white flex flex-row space-x-2 items-center md:px-3 px-2 py-2 focus:outline-none focus:ring-1 rounded text-white opacity-80 hover:opacity-100 cursor-pointer"
+              classList={{
+                'rounded-none	active:bg-gray-300 hover:bg-gray-300 focus:outline-none focus:highlight-none active:highlight-none focus:ring-0 active:outline-none':
+                  showMenu(),
+              }}
               title="Import from JSON"
             >
               <input type="file" class="sr-only" onChange={uploadFile} accept=".json" />
@@ -117,6 +125,10 @@ export const Header: Component<{
               type="button"
               onClick={() => exportToJSON(props.tabs)}
               class="text-black md:text-white flex flex-row space-x-2 items-center md:px-3 px-2 py-2 focus:outline-none focus:ring-1 rounded text-white opacity-80 hover:opacity-100"
+              classList={{
+                'rounded-none	active:bg-gray-300 hover:bg-gray-300 focus:outline-none focus:highlight-none active:highlight-none focus:ring-0 active:outline-none':
+                  showMenu(),
+              }}
               title="Export to JSON"
             >
               <svg class="fill-current h-6" viewBox="0 0 24 24">
@@ -129,6 +141,10 @@ export const Header: Component<{
               type="button"
               onClick={() => exportToCsb(props.tabs)}
               class="text-black md:text-white flex flex-row space-x-2 items-center md:px-3 px-2 py-2 focus:outline-none focus:ring-1 rounded text-white opacity-80 hover:opacity-100"
+              classList={{
+                'rounded-none	active:bg-gray-300 hover:bg-gray-300 focus:outline-none focus:highlight-none active:highlight-none focus:ring-0 active:outline-none':
+                  showMenu(),
+              }}
               title="Export to CodeSandbox"
             >
               <svg
@@ -148,6 +164,8 @@ export const Header: Component<{
               classList={{
                 'opacity-80 hover:opacity-100': !copy(),
                 'text-green-100': copy() && !showMenu(),
+                'rounded-none	active:bg-gray-300 hover:bg-gray-300 focus:outline-none focus:highlight-none active:highlight-none focus:ring-0 active:outline-none':
+                  showMenu(),
               }}
               title="Share with a minified link"
             >
