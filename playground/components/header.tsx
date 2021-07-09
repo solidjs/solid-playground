c30d0331cca245b28b4ecf989d1c88ed895ec06e
Stephen Charbonneau <46049787+istareatscreens@users.noreply.github.com>
2021-07-08 20:17:10 -0400

    Update playground/components/header.tsx
    Replace ternary with <Show /> component
    
    Co-authored-by: Alexandre <amoutonbrady@gmail.com>

--- playground/components/header.tsx
+++ playground/components/header.tsx
@@ -175,7 +175,9 @@ export const Header: Component<{
           title="Mobile Menu Button"
           onClick={() => setShowMenu(!showMenu())}
         >
-          {showMenu() ? (
+          <Show when={showMenu()} fallback={<Icon path={menu}  class="h-6 w-6" />}> 
+              <Icon path={xCircle}  class="h-6 w-6" />
+          </Show>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="h-6 w-6"
