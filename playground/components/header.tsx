c54c928d9ea359e9ffa658f29d2c0bf7b09e3ba8
istareatscreens <schar138@uottawa.ca>
2021-07-08 22:01:41 -0400

    Change svgs to icon import, fix header size bug
    Correct header size change on mobile menu click by accounting for
    border size on xClose by changing border padding
    
    Change sun and moon svg to icon imports

--- playground/components/header.tsx
+++ playground/components/header.tsx
@@ -1,6 +1,14 @@
 import { Component, onCleanup, createSignal, JSX, Show, createMemo, For } from 'solid-js';
 import { Icon } from '@amoutonbrady/solid-heroicons';
-import { share, link, upload } from '@amoutonbrady/solid-heroicons/outline';
+import {
+  share,
+  link,
+  upload,
+  xCircle,
+  menu,
+  moon,
+  sun,
+} from '@amoutonbrady/solid-heroicons/outline';
 
 import logo from '../assets/logo.svg?url';
 import { processImport, Tab } from '../../src';
@@ -90,27 +98,8 @@ export const Header: Component<{
               class="text-black md:text-white flex flex-row space-x-2 items-center md:px-3 px-2 py-2 focus:outline-none focus:ring-1 rounded text-white opacity-80 hover:opacity-100"
               title="Toggle dark mode"
             >
-              <Show
-                when={props.dark}
-                fallback={
-                  <svg class="h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                    />
-                  </svg>
-                }
-              >
-                <svg class="h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                </svg>
+              <Show when={props.dark} fallback={<Icon path={moon} class="h-6" />}>
+                <Icon path={sun} class="h-6" />
               </Show>
               <span class="text-xs md:sr-only">{props.dark ? 'Light' : 'Dark'} mode</span>
             </button>
@@ -170,44 +159,16 @@ export const Header: Component<{
         <button
           type="button"
           id="menu-btn"
+          classList={{
+            'border-white border': showMenu(),
+          }}
           class="z-40 px-3 py-2 focus:outline-none focus:ring-1 rounded text-white opacity-80 hover:opacity-100 visible relative md:hidden m-0 mr-[10px]"
-          classList={{ 'border-white border': showMenu() }}
           title="Mobile Menu Button"
           onClick={() => setShowMenu(!showMenu())}
         >
-          <Show when={showMenu()} fallback={<Icon path={menu}  class="h-6 w-6" />}> 
-              <Icon path={xCircle}  class="h-6 w-6" />
+          <Show when={showMenu()} fallback={<Icon path={menu} class="h-6 w-6" />}>
+            <Icon path={xCircle} class="h-[22px] w-[22px]" /* adjusted to account for border */ />
           </Show>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width={2}
-                d="M4 6h16M4 10h16M4 14h16M4 18h16"
-              />
-            </svg>
-          )}
           <span class="sr-only text-black md:text-white">Export to JSON</span>
         </button>
         <select
