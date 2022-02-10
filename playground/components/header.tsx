60d886abfcf7513d441b2d385a3bc60f255abdca
Marvin Rudolph <rudolph.marvin@web.de>
2022-02-10 13:47:52 +0100

    refactor: use icon from solid-heroicons
    Co-authored-by: Alexandre <amoutonbrady@gmail.com>

--- playground/components/header.tsx
+++ playground/components/header.tsx
@@ -95,20 +95,7 @@ export const Header: Component<{
               }}
               title="Format current document"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
+              <Icon  path={menuAlt2}  class="h-6 w-6"  />
               <span class="text-xs md:sr-only">Format current document</span>
             </button>
 
