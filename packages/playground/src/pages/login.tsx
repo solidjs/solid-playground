ad2be9fa477c5dd3f9c7a2166bb0988f26d6daab
modderme123 <modderme123@gmail.com>
2022-11-01 18:32:38 -0400

    legacy build
    fixes

--- packages/playground/src/pages/login.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, useSearchParams } from '@solidjs/router';
-import { useAppContext } from '../context';
-
-export const Login = () => {
-  const [params] = useSearchParams();
-  const context = useAppContext()!;
-  context.token = params.token;
-  return <Navigate href="/" />;
-};
