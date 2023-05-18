09262ca48822c08f53afe98dd2e3bb7bc968e8a0
modderme123 <modderme123@gmail.com>
2023-05-18 11:40:47 -0700

    cleanups

--- src/components/repl.tsx
+++ src/components/repl.tsx
@@ -398,7 +398,7 @@ const Repl: ReplProps = (props) => {
             />
           </Match>
           <Match when={outputTab() == 1}>
-            <section class="relative flex min-h-0 min-w-0 flex-col divide-y-2 divide-slate-200 dark:divide-neutral-800">
+            <section class="relative flex min-h-0 min-w-0 flex-1 flex-col divide-y-2 divide-slate-200 dark:divide-neutral-800">
               <Editor
                 url={`file:///${props.id}/output_dont_import.tsx`}
                 isDark={props.dark}
