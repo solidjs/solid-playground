f498147ec7935c9ded159aea686599b00cc15b1b
Tito <tito.bouzout@gmail.com>
2023-09-03 12:41:50 -0300

    small improvement to default example
    The default example includes `setCount(count()+1)`
    Probably unharmful, but a bad practice, because `count()` will cause tracking, for the sake of education I believe `setCount(count => count+1)` would be better

--- packages/solid-repl/src/index.ts
+++ packages/solid-repl/src/index.ts
@@ -5,7 +5,7 @@ import { createSignal } from "solid-js";
 
 function Counter() {
   const [count, setCount] = createSignal(1);
-  const increment = () => setCount(count() + 1);
+  const increment = () => setCount(count => count + 1);
 
   return (
     <button type="button" onClick={increment}>
