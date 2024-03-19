2349a2be47e0e9419f813810df5f9b3260183d6c
Birk Skyum <birk.skyum@pm.me>
2024-03-20 00:29:28 +0100

    bump more deps

--- packages/playground/src/components/setupSolid.ts
+++ packages/playground/src/components/setupSolid.ts
@@ -1,6 +1,6 @@
 import { languages } from 'monaco-editor';
 
-const solidTypes = import.meta.glob('/node_modules/{solid-js,csstype}/**/*.{d.ts,json}', { eager: true, as: 'raw' });
+const solidTypes: Record<string, string> = import.meta.glob('/node_modules/{solid-js,csstype}/**/*.{d.ts,json}', { eager: true, query: '?raw', import:"default" });
 
 for (const path in solidTypes) {
   languages.typescript.typescriptDefaults.addExtraLib(solidTypes[path], `file://${path}`);
