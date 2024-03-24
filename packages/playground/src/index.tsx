018f44a04ba7be0163e85664cb2860f821e4e414
Birk Skyum <birk.skyum@pm.me>
2024-03-24 12:18:06 +0100

    cleanup imports

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -1,7 +1,5 @@
-import { Router } from '@solidjs/router';
 import { render } from 'solid-js/web';
 import { App } from './app';
-import { AppContextProvider } from './context';
 import { registerServiceWorker } from './utils/serviceWorker';
 import 'virtual:uno.css';
 import 'solid-repl/repl/main.css';
