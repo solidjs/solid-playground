f2a68b77e98a9585e975716bf342258b5b4df0e0
Caleb Taylor <aquaductape@gmail.com>
2024-03-25 17:53:56 -0700

    cleanup

--- packages/solid-repl/src/components/preview.tsx
+++ packages/solid-repl/src/components/preview.tsx
@@ -5,17 +5,17 @@ import { GridResizer } from './gridResizer';
 const dispatchKeyboardEventToParentZoomState = () => `
   document.addEventListener('keydown', (e) => {
     if (!(e.ctrlKey || e.metaKey)) return;
-    if(!['=', '-'].includes(e.key)) return
+    if (!['=', '-'].includes(e.key)) return;
 
     const options = {
       key: e.key,
       ctrlKey: e.ctrlKey,
-      metaKey: e.metaKey
-    }
-    const keyboardEvent = new KeyboardEvent('keydown', options)
-    window.parent.document.dispatchEvent(keyboardEvent)
+      metaKey: e.metaKey,
+    };
+    const keyboardEvent = new KeyboardEvent('keydown', options);
+    window.parent.document.dispatchEvent(keyboardEvent);
 
-    e.preventDefault()
+    e.preventDefault();
   });
 `;
 
