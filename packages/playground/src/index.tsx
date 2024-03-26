4933321c4c6e3c862ac5e307f8f95af345f4bdb9
Caleb Taylor <aquaductape@gmail.com>
2024-03-25 17:57:57 -0700

    rearrange styling to prevent tailwind reset to override user styles

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -1,8 +1,8 @@
 import { render } from 'solid-js/web';
 import { App } from './app';
 import { registerServiceWorker } from './utils/serviceWorker';
-import 'virtual:uno.css';
 import 'solid-repl/repl/main.css';
+import 'virtual:uno.css';
 
 render(() => <App />, document.querySelector('#app')!);
 
