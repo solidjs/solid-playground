6c073bfa18e467bcba07d2dabcd668c37b4bc578
modderme123 <modderme123@gmail.com>
2024-05-11 22:51:58 -0700

    tsc issues?

--- packages/playground/src/pages/login.tsx
+++ packages/playground/src/pages/login.tsx
@@ -4,6 +4,6 @@ import { useAppContext } from '../context';
 export const Login = () => {
   const [params] = useSearchParams();
   const context = useAppContext()!;
-  context.token = params.token;
+  context.token = params.token!;
   return <Navigate href="/" />;
 };
